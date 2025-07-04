7e43cb535ad3e292c52ede0891ee8ae882d7d493
TICNET\student2 <tky2302126@stu.o-hara.ac.jp>
2025-07-04 15:10:56 +0900

    add : mouseクラス

---
+++ Mouse.cpp
@@ -0,0 +1,64 @@
+﻿#include "Mouse.h"
+
+Mouse::Mouse()
+{
+	mouseEnable = false;
+}
+
+Mouse::~Mouse()
+{
+}
+
+void Mouse::MouseInfoUpdate()
+{
+	if(!mouseEnable)
+	{
+		mouseInfo.position = { -1, -1 };
+		mouseInfo.leftButton = InputState::Waiting;
+		mouseInfo.rightButton = InputState::Waiting;
+		return;
+	}
+
+	GetMousePoint(&mouseInfo.position.x, &mouseInfo.position.y);
+
+#pragma region 左クリック
+	auto&  LeftButton = mouseInfo.leftButton;
+	if((LeftButton == InputState::Waiting || LeftButton == InputState::Canceled) && IsMouseLeftButtonPressed())
+	{
+		LeftButton = InputState::Started;
+	}
+	else if(LeftButton == InputState::Started && IsMouseLeftButtonPressed())
+	{
+		LeftButton = InputState::Performed;
+	}
+	else if((LeftButton == InputState::Performed || LeftButton == InputState::Started) && !IsMouseLeftButtonPressed())
+	{
+		LeftButton = InputState::Canceled;
+	}
+	else if(LeftButton == InputState::Canceled && ! IsMouseLeftButtonPressed())
+	{
+		LeftButton = InputState::Waiting;
+	}
+#pragma endregion
+
+#pragma region 右クリック
+	auto& RightButton = mouseInfo.rightButton;
+	if ((RightButton == InputState::Waiting || RightButton == InputState::Canceled) && IsMouseRightButtonPressed())
+	{
+		RightButton = InputState::Started;
+	}
+	else if (RightButton == InputState::Started && IsMouseRightButtonPressed())
+	{
+		RightButton = InputState::Performed;
+	}
+	else if ((RightButton == InputState::Performed || RightButton == InputState::Started) && !IsMouseRightButtonPressed())
+	{
+		RightButton = InputState::Canceled;
+	}
+	else if (RightButton == InputState::Canceled && !IsMouseRightButtonPressed())
+	{
+		RightButton = InputState::Waiting;
+	}
+#pragma endregion
+
+}
---
+++ Mouse.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+//! 循環参照注意
+#include "InputSystem.h"
+
+/**
+* @struct Vector2Int
+* @brief 2次元座標を管理する構造体
+*/
+struct Vector2Int
+{
+	int x;
+	int y;
+
+	bool operator== (const Vector2Int& other) const { return x == other.x && y == other.y; }
+	bool operator!=(const Vector2Int& other) const { return !(*this == other); }
+};
+
+/**
+* @struct MouseInfo
+* @brief マウス情報
+*/
+struct MouseInfo
+{
+	Vector2Int position;
+	InputState leftButton;  // 左クリック
+	InputState rightButton; // 右クリック
+};
+
+//
+class Mouse
+{
+public:
+	Mouse();
+	~Mouse();
+
+	void MouseInfoUpdate();
+
+	MouseInfo GetMouseInfo() const { return mouseInfo; }
+
+	// クリックしたかどうかを返すヘルパー関数
+	bool IsMouseLeftButtonClicked() { return mouseInfo.leftButton == InputState::Started; }
+	bool IsMouseRightButtonClicked() { return mouseInfo.rightButton == InputState::Started; }
+
+private:
+	
+	// @brief	    マウスの入力情報
+	// @History		25/07/04 作成(h_Suzuki)
+	MouseInfo mouseInfo;
+
+	bool mouseEnable;
+
+	bool IsMouseLeftButtonPressed() { return GetMouseInput() && MOUSE_INPUT_LEFT; }
+	bool IsMouseRightButtonPressed() { return GetMouseInput() && MOUSE_INPUT_RIGHT; }
+
+#pragma region マウス関係
+public:
+	void MouseEnable() { mouseEnable = true; }
+
+	void MouseDisable() { mouseEnable = false; }
+};
+
